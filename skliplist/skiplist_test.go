aeaf8d80f594a198f6dc6ddb92e1dda92b1552c9
wangfuwei01 <825583326@qq.com>
2021-06-28 21:24:25 +0800

    test skiplist

---
+++ skliplist/skiplist_test.go
@@ -0,0 +1,13 @@
+package skliplist
+
+import "testing"
+
+func TestSkipList(t *testing.T) {
+	slt := NewSkipList()
+	for i := 10; i > 0; i-- {
+		slt.Insert(i)
+		//slt.PrintSkipList()
+	}
+
+	slt.PrintSkipList()
+}
---
+++ stategraph/stategraph.go
@@ -0,0 +1,56 @@
+package stategraph
+
+import "fmt"
+
+//状态接口
+type IState interface {
+	Start()
+	Update()
+	Stop()
+	Value() string
+}
+
+type FSM struct {
+	state      IState
+	registers  map[string]IState
+	def_state  string
+	input_hour uint32
+}
+
+func NewStateMachine() *FSM {
+	fsm := &FSM{}
+	fsm.registers = make(map[string]IState)
+	return fsm
+}
+
+//
+func (fsm *FSM) Process(hour uint32) error {
+	fsm.input_hour = hour
+	if fsm.state == nil {
+
+	} else {
+		fsm.state.Update()
+	}
+	return nil
+}
+
+//切换状态
+func (fsm *FSM) ChangeState(value string) {
+	if fsm.state == nil {
+		fsm.state = fsm.registers[value]
+	} else if fsm.state.Value() != value {
+		fsm.state.Stop()
+		fsm.state = fsm.registers[value]
+	} else {
+		fmt.Printf("same state\n")
+		return
+	}
+
+	fsm.state.Start()
+}
+
+//
+type StateBase struct {
+	fsm   *FSM
+	value string
+}
